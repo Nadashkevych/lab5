1d6e716058956ac00b56232b2fb457ed4a9e55a0
Volt <sonianadashkevich@gmail.com>
2020-05-12 09:21:07 +0200

    all tests passed dodano testy dla wszystkich mozliwych przypadkow

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -7,23 +7,53 @@ public class Tests {
     private HalfString halfString = new HalfString();
     private ReverseArray rArray = new ReverseArray();
 
+    //zadanie1
     @Test
     public void if1TempLowerThan100ShouldReturnTrue() {
         Assertions.assertTrue(temperature.lowerThan100(40, 50));
     }
 
     @Test
+    public void if1TempHigherThan100ShouldReturnFalse() {
+        Assertions.assertFalse(temperature.lowerThan100(440, 150));
+    }
+
+    //zadanie2
+    @Test
     public void ifSequenceFoundShouldReturnTrue() {
         Assertions.assertTrue(search.findSequence(new int[]{1, 2, 3, 3, 7}));
     }
 
     @Test
-    public void ShouldBeEqualToHalfOfString() {
+    public void ifSequenceNotFoundShouldReturnFalse() {
+        Assertions.assertFalse(search.findSequence(new int[]{1, 5, 3, 3, 7}));
+    }
+
+    @Test
+    public void ifArrayNullShouldReturnFalse() {
+        Assertions.assertFalse(search.findSequence(new int[5]));
+    }
+
+    //zadanie3
+    @Test
+    public void shouldBeEqualToHalfOfEvenString() {
         Assertions.assertEquals("Str", halfString.halfString("String"));
     }
 
     @Test
+    public void ShouldBeEqualToSmallerHalfOfOddString() {
+        Assertions.assertEquals("Str", halfString.halfString("Strings"));
+    }
+
+    @Test
+    public void IfNullShouldBeEqualToEmpty() {
+        Assertions.assertEquals("", halfString.halfString(""));
+    }
+
+    //zadanie4
+    @Test
     public void ArrayShouldBeEqualToReverse() {
         Assertions.assertArrayEquals(new int[]{1, 3, 5, 7}, rArray.reverseArray(new int[]{7, 5, 3, 1}));
     }
+
 }
