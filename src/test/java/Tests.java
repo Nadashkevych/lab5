009e95623b97b961558ba958c04c2550ac72ad50
Volt <sonianadashkevich@gmail.com>
2020-05-12 09:27:54 +0200

    all tests passed dodano testy dla wszystkich mozliwych przypadkow

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -7,7 +7,8 @@ public class Tests {
     private HalfString halfString = new HalfString();
     private ReverseArray rArray = new ReverseArray();
 
-    //zadanie1
+    //zadanie1 - testuje sie metode, ktora zwraca true jesli temperatura<100 lub
+    // false jesli wieksza
     @Test
     public void if1TempLowerThan100ShouldReturnTrue() {
         Assertions.assertTrue(temperature.lowerThan100(40, 50));
@@ -18,7 +19,8 @@ public class Tests {
         Assertions.assertFalse(temperature.lowerThan100(440, 150));
     }
 
-    //zadanie2
+    //zadanie2 - testuje sie metode, szukajaca sekwencje liczb 1 2 3; musi zwracac true
+    // jesli sekwencja w tablicy wystapi lub false jesli nie
     @Test
     public void ifSequenceFoundShouldReturnTrue() {
         Assertions.assertTrue(search.findSequence(new int[]{1, 2, 3, 3, 7}));
@@ -34,7 +36,8 @@ public class Tests {
         Assertions.assertFalse(search.findSequence(new int[5]));
     }
 
-    //zadanie3
+    //zadanie3 - zwraca polowe stringa o parzystej liczbie znakow, mniejsza czesc
+    // stringa o nieparzystej liczbie znakow lub pusty string w przypadku przekazania null
     @Test
     public void shouldBeEqualToHalfOfEvenString() {
         Assertions.assertEquals("Str", halfString.halfString("String"));
@@ -50,7 +53,7 @@ public class Tests {
         Assertions.assertEquals("", halfString.halfString(""));
     }
 
-    //zadanie4
+    //zadanie4 - metoda musi zwracac tablice int'ow w odwrotnej kolejnosci
     @Test
     public void ArrayShouldBeEqualToReverse() {
         Assertions.assertArrayEquals(new int[]{1, 3, 5, 7}, rArray.reverseArray(new int[]{7, 5, 3, 1}));
